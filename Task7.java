4aca0a9a5ed5977cdbddbce0b5dc8dc562c46184
TReFtreef <53417222+TReFtreef@users.noreply.github.com>
2019-09-10 15:52:27 +0300

    Задача 7

---
+++ Task7.java
@@ -0,0 +1,11 @@
+package Training.Tasks;
+
+public class Task7 {
+    public static void main(String [] args){
+        int sum = 0;
+        for(int i = 20; i<200; i++){
+            sum+=i;
+        }
+        System.out.println(sum);
+    }
+}
